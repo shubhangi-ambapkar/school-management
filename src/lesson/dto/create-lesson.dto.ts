6faf2d4a6d8b5d0086a481fc06035e10c27520b3
Shubhangi Ambapkar <ambapkarshubhangi@gmail.com>
2025-02-16 20:06:00 +0530

    Adding validations for Create DTO

--- src/lesson/dto/create-lesson.dto.ts
+++ src/lesson/dto/create-lesson.dto.ts
@@ -1,16 +1,20 @@
 import { Field, ID, InputType } from "@nestjs/graphql";
+import { IsDateString, MinLength } from "class-validator";
 
 @InputType()
 export class CreateLessonDto {
   @Field((type) => ID)
   id: string;
 
+  @MinLength(1)
   @Field()
   name: string;
 
+  @IsDateString()
   @Field()
   startDate: string;
 
+  @IsDateString()
   @Field()
   endDate: string;
 }
